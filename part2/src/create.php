e51eea9bbb814a44bcfd2df1ce5ded31b7146709
canadu <canacana2@gmail.com>
2021-09-23 21:43:11 +0900

    バリデーション処理を追加＆Bootstrapをインストール

--- part2/src/create.php
+++ part2/src/create.php
@@ -25,6 +25,32 @@ EOT;
     }
 }
 
+function validate($book)
+{
+    $errors = [];
+
+    if (!strlen($book['bookName'])) {
+        $errors['bookName'] = "本の名前を入力してください。";
+    } elseif (strlen($book['bookName']) > 100) {
+        $errors['bookName'] = "本の名前を入力してください。";
+    }
+    if (!strlen($book['authorName'])) {
+        $errors['authorName'] = "著者名の名前を入力してください。";
+    } elseif (strlen($book['authorName']) > 100) {
+        $errors['authorName'] = "著者名の名前を入力してください。";
+    }
+
+    if (!strlen($book['evaluation'])) {
+        $errors['evaluation'] = "評価を入力してください。";
+    } elseif (!is_numeric($book['evaluation'])) {
+        $errors['evaluation'] = "数字で入力してください。";
+    } elseif ((int)($book['evaluation']) <= 0 && (int)($book['evaluation']) <= 5) {
+        $errors['evaluation'] = "数字は0から5の間で入力してください";
+    }
+
+    return $errors;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $book = [
         'bookName' => $_POST['bookName'],
@@ -33,12 +59,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'evaluation' => $_POST['evaluation'],
         'thoughts' => $_POST['thoughts']
     ];
-    var_dump($book);
-    $link = dbConnect();
-    var_dump($link);
-    createBooklog($link, $book);
-    var_dump(100);
-    mysqli_close($link);
+    $errors = validate($book);
+    if (!count($errors)) {
+        $link = dbConnect();
+        createBooklog($link, $book);
+        mysqli_close($link);
+        header("Location:index.php");
+    }
 }
-
-header("Location:index.php");
+include  'views/new.php';
--- part2/src/new.php
+++ part2/src/new.php
@@ -1,51 +1,10 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>読書ログの登録</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <h3>読書ログの記録</h3>
-    <form action="create.php" method="post">
-        <div>
-            <label for="bookName">書籍名</label>
-            <input type="text" id="bookName" name="bookName">
-        </div>
-        <div>
-            <label for="authorName">著者名</label>
-            <input type="text" id="authorName" name="authorName">
-        </div>
-        <div>
-            <input type="radio" name="status" id="unRead" value="unRead">
-            <label for="unRead">未読</label>
-        </div>
-        <div>
-            <input type="radio" name="status" id="Reading" value="Reading">
-            <label for="Reading">読んでいる</label>
-        </div>
-        <div>
-            <input type="radio" name="status" id="Read" value="Read">
-            <label for="Read">読了</label>
-        </div>
-        <div>
-            <label for="evaluation">評価（５点満点の整数）</label>
-            <input type="text" id="evaluation" name="evaluation">
-        </div>
-        <div>
-            <label for="thoughts">感想</label>
-            <textarea name="thoughts" id="thoughts" rows="10"></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-
-
-</body>
-
-</html>
+$errors = [];
+$book = [
+    'bookName' => '',
+    'authorName' => '',
+    'status' => '',
+    'evaluation' => '',
+    'thoughts' => '',
+];
+include 'views/new.php';
