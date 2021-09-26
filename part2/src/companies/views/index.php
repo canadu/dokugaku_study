0023cb9d2166e143c5094bdcfafc76be6dfbf3d2
canadu <canacana2@gmail.com>
2021-09-26 22:24:17 +0900

    共通部品化

---
+++ part2/src/companies/views/index.php
@@ -0,0 +1,16 @@
+<?php
+// include 'layout.php';
+// $title = '会社情報の一覧';
+?>
+<h1>会社情報の一覧</h1>
+<a href="new.php">会社情報を登録する</a>
+<main>
+    <section>
+        <h2>株式会社メルカリ</h2>
+        <div>創業2013年｜山田</div>
+    </section>
+    <section>
+        <h2>株式会社ヤフー</h2>
+        <div>創業2013年｜孫正義</div>
+    </section>
+</main>
--- part2/src/companies/views/new.php
+++ part2/src/companies/views/new.php
@@ -1,39 +1,28 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="stylesheets/css/app.css">
-    <title>会社情報の登録</title>
-</head>
-
-<body>
-    <div class="container">
-        <h1 class="h2 text-dark mt-4 mb-4">会社情報の登録</h1>
-        <form action="create.php" method="post">
-            <?php if (count($errors)) : ?>
-                <ul class="text-danger">
-                    <?php foreach ($errors  as $error) : ?>
-                        <li><?php echo $error; ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-            <div class="form-group">
-                <label for="name">会社名</label>
-                <input type="text" id="name" class="form-control" name="name" value="<?php echo $company['name']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="establishment_date">設立日</label>
-                <input type="date" id="establishment_date" class="form-control" name="establishment_date" value="<?php echo $company['establishment_date']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="founder">代表者</label>
-                <input type="text" id="founder" class="form-control" name="founder" value="<?php echo $company['founder']; ?>">
-            </div>
-            <button type="submit" class="btn btn-primary">登録する</button>
-        </form>
+<?php
+// include 'layout.php';
+// $title = '会社情報の登録';
+?>
+<h1 class="h2 text-dark mt-4 mb-4">会社情報の登録</h1>
+<form action="create.php" method="post">
+    <?php if (count($errors)) : ?>
+        <ul class="text-danger">
+            <?php foreach ($errors  as $error) : ?>
+                <li><?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <div class="form-group">
+        <label for="name">会社名</label>
+        <input type="text" id="name" class="form-control" name="name" value="<?php echo $company['name']; ?>">
     </div>
-</body>
-
-</html>
+    <div class="form-group">
+        <label for="establishment_date">設立日</label>
+        <input type="date" id="establishment_date" class="form-control" name="establishment_date" value="<?php echo $company['establishment_date']; ?>">
+    </div>
+    <div class="form-group">
+        <label for="founder">代表者</label>
+        <input type="text" id="founder" class="form-control" name="founder" value="<?php echo $company['founder']; ?>">
+    </div>
+    <button type="submit" class="btn btn-primary">登録する</button>
+</form>
+</div>
