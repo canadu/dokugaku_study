cd014d9402c354056b0359779fca52d9929a8d97
canadu <canacana2@gmail.com>
2021-09-27 15:28:39 +0900

    最新取得

--- part2/src/create.php
+++ part2/src/create.php
@@ -19,11 +19,36 @@ function createBooklog($link, $book)
     )
 EOT;
     $result = mysqli_query($link, $sql);
-    var_dump($result);
     if (!$result) {
         error_log('Error: fail to create review');
         error_log('Debugging error:' . mysqli_error($link));
     }
+}
+
+function validate($book)
+{
+    $errors = [];
+
+    if (!strlen($book['bookName'])) {
+        $errors['bookName'] = "本の名前を入力してください。";
+    } elseif (strlen($book['bookName']) > 100) {
+        $errors['bookName'] = "本の名前を入力してください。";
+    }
+    if (!strlen($book['authorName'])) {
+        $errors['authorName'] = "著者名の名前を入力してください。";
+    } elseif (strlen($book['authorName']) > 100) {
+        $errors['authorName'] = "著者名の名前を入力してください。";
+    }
+
+    if (!strlen($book['evaluation'])) {
+        $errors['evaluation'] = "評価を入力してください。";
+    } elseif (!is_numeric($book['evaluation'])) {
+        $errors['evaluation'] = "数字で入力してください。";
+    } elseif ((int)($book['evaluation']) <= 0 && (int)($book['evaluation']) <= 5) {
+        $errors['evaluation'] = "数字は0から5の間で入力してください";
+    }
+
+    return $errors;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -34,10 +59,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'evaluation' => $_POST['evaluation'],
         'thoughts' => $_POST['thoughts']
     ];
-    
-    $link = dbConnect();
-    createBooklog($link, $book);
-    mysqli_close($link);
+    $errors = validate($book);
+    if (!count($errors)) {
+        $link = dbConnect();
+        createBooklog($link, $book);
+        mysqli_close($link);
+        header("Location:index.php");
+    }
 }
-
-header("Location:index.php");
+include  'views/new.php';
---
+++ part2/src/views/new.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/css/app.css">
+    <title>読書ログの登録</title>
+</head>
+
+<body>
+    <header class="navbar shadow-sm p-3 mb-5 bg-white">
+        <h1 class="h2">
+            <a class="text-body text-decoration-none" href="index.php">読書ログ</a>
+        </h1>
+    </header>
+
+    <div class=" container">
+
+        <h2 class="h3 text-dark mb-4">読書ログの登録</h2>
+
+        <form action="create.php" method="POST">
+            <?php if (count($errors)) : ?>
+                <ul class="text-danger">
+                    <?php foreach ($errors as $error) : ?>
+                        <li><?php echo ($error); ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+
+            <div class="form-group">
+                <label for="bookName">書籍名</label>
+                <input type="text" id="bookName" class="form-control" name="bookName" value="<?php echo $book['bookName']; ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="authorName">著者名</label>
+                <input type="text" id="authorName" class="form-control" name="authorName" value="<?php echo $book['authorName']; ?>">
+            </div>
+
+            <!-- ラジオボタン -->
+            <div class="form-group">
+                <label>読書状況</label>
+                <div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="status" class="form-check-input" id="unRead" value="unRead" <?php echo ($book['status'] === 'unRead') ? ('checked') : (''); ?>>
+                        <label for="unRead" class="form-check-label">未読</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="status" class="form-check-input" id="Reading" value="Reading" <?php echo ($book['status'] === 'Reading') ? ('checked') : (''); ?>>
+                        <label for="Reading" class="form-check-label">読んでいる</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="status" class="form-check-input" id="Read" value="Read" <?php echo ($book['status'] === 'Read') ? ('checked') : (''); ?>>
+                        <label for="Read" class="form-check-label">読了</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="evaluation">評価（５点満点の整数）</label>
+                <input type="text" id="evaluation" class="form-control" name="evaluation" value="<?php echo $book['evaluation']; ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="thoughts">感想</label>
+                <textarea name="thoughts" id="thoughts" class="form-control" rows="10" value="<?php echo $book['thoughts']; ?>"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">登録する</button>
+        </form>
+    </div>
+</body>
+
+</html>
