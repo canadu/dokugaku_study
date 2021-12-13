e7dd25e9ba6b933ff98cfac022d0d6348952d880
canadu <canacana2@gmail.com>
2021-12-13 17:35:40 +0900

    bootstrap

--- part2/src/memo/views/index.php
+++ part2/src/memo/views/index.php
@@ -1,35 +1,18 @@
 <main>
     <div class="row">
-        <!-- 新規作成用 -->
-        <div class="col-md-4">
-            <!-- カードが入ります01 -->
-            <div class="card mb-3">
-                <!-- カードの本文エリア -->
-                <div class="card-body d-flex justify-content-between">
-                    <!-- <h4 class="cartd-title class="btn btn-secondary"">てすてす１</h4> -->
-                    <a class="btn btn-primary" href="#">新規追加</a>
-                </div>
-                <div class="card-footer footer-container">
-                    <span>2021年12月3日12:00</span>
-                    <div class="icon">
-                        <a href="#" title="メモを編集"><i class="fas fa-edit"></i></a>
-                        <a href="#" title="メモを削除"><i class="fas fa-trash-alt"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
         <?php if (count($results) > 0): ?>
             <?php foreach ($results as $result) : ?>
                 <div class="col-md-4">
-                    <!-- カードが入ります01 -->
                     <div class="card mb-3">
                         <!-- カードの本文エリア -->
-                        <div class="card-body d-flex justify-content-between">
-                            <h4 class="cartd-title class="btn btn-secondary""><?php echo $result['title']; ?></h4>
-                            <a class="btn btn-secondary" href="#">詳しく見る</a>
-                        </div>
+                        <a href="#" class="card-body text-dark">
+                            <!-- タイトル -->
+                            <h5 class="card-title"><?php echo $result['title']; ?></h5>
+                            <!-- メモ詳細 -->
+                            <p class="card-text text-truncate"><?php echo $result['detail']; ?></p>
+                        </a>
                         <div class="card-footer footer-container">
-                            <span><?php echo $result['create_at']; ?></span>
+                            <small class="text-muted"><?php echo $result['create_at']; ?></small>
                             <div class="icon">
                                 <a href="#" title="メモを編集"><i class="fas fa-edit"></i></a>
                                 <a href="#" title="メモを削除"><i class="fas fa-trash-alt"></i></a>
--- part2/src/memo/views/layout.php
+++ part2/src/memo/views/layout.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,21 +8,29 @@
     <link rel="stylesheet" href="stylesheets/css/custom.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-
     <title><?php echo $title; ?></title>
 </head>
 <body>
-    <nav class="navbar shadow-sm p-3 mb-5 bg-white">
-        <a class="navbar-brand" href="#">
-            <img src="" alt="" width="30" height="30" class="d-inline-block align-top">memo
-        </a>
-        <form class="form-inline">
-            <input class="form-control mr-md-2" type="search"　placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-primary my-2 my-md-0" type="submit">検索</button>
-        </form>
+    <nav class="navbar navbar-expand navbar-dark bg-dark mb-4">
+        <a class="navbar-brand" href="#">memo</a> 
+        <!-- ハンバーガーメニュー     -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar"
+            aria-controls="myNavbar" aria-expanded="false" aria-label="ハンバーガーメニュー">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="myNavbar">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item"><a class="nav-link" href="new_user"><i class="fas fa-user"></i> Sign Up</a></li>
+                <li class="nav-item"><a class="nav-link" href="login"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+            </ul>
+        </div>
     </nav>
+    <!-- ここまで！！ -->
     <div class=" container">
         <?php include $content; ?>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
